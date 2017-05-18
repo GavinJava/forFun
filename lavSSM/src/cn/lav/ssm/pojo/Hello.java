af873e6ea6fff6cb7108f2eb3b3a2847ac7fa816
HongyuYao <1014394460@qq.com>
2017-05-18 19:49:51 +0800

    Change hello to Hello,and delete the helpless annotation.Add a page named HelloGit.jsp.(By Yao,May 18.2017 at 19:49)

---
+++ lavSSM/src/cn/lav/ssm/pojo/Hello.java
@@ -0,0 +1,17 @@
+package cn.lav.ssm.pojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Hello {
+	
+	@Test
+	public void testSayHello() throws Exception {
+		System.out.println("hello Git...");
+	}
+}
